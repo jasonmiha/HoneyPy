2fa1f9d795b14c6ed69891a3cfe962c001003c38
Iason Mihalopoulos <imihalo1@jh.edu>
2025-07-28 15:51:24 -0700

    Add SSH server functionality to honeypot
    - Integrated `paramiko` to create a custom SSH server interface.
    - Implemented authentication and channel request handling methods.
    - Allowed only password authentication with predefined credentials.
    - Approved shell and PTY requests for client interactions.

--- ssh_honeypot.py
+++ ssh_honeypot.py
@@ -2,6 +2,7 @@
 import logging
 from logging.handlers import RotatingFileHandler
 import socket
+import paramiko
 
 # Constants
 logging_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
@@ -53,5 +54,40 @@ def emulated_shell(channel, client_ip):     # Define the emulated shell function
 
 
 # SSH Server + Sockets
+
+class Server(paramiko.ServerInterface):
+
+    def __init__(self, client_ip, input_username=None, input_password=None):
+        self.client_ip = client_ip
+        self.input_username = input_username
+        self.input_password = input_password
+
+    def check_channel_request(self, kind:str, chanid: int) -> int:
+        # Check if the requested channel type is a 'session', which allows for shell or command execution
+        if kind == 'session':
+            return paramiko.OPEN_SUCCEEDED
+    
+    def get_allowed_auths(self):
+        # Specify that only 'password' authentication is allowed
+        return "password"
+    
+    def check_auth_password(self, username: str, password: str) -> int:
+        # Validate the provided username and password against expected values
+        if self.input_username is not None and self.input_password is not None:
+            if username == 'username' and password == 'password':
+                return paramiko.AUTH_SUCCESSFUL
+            else:
+                return paramiko.AUTH_FAILED
+    
+    def check_channel_shell_request(self, channel) -> int:  # Check if the client is requesting a shell
+        self.event.set()
+        return True
+   
+    def check_channel_pty_request(self, channel, term, width, height, pixel_width, pixel_height) -> int:    # Approve the pseudo-terminal (PTY) request
+        return True
+    
+    def check_channel_exec_request(self, channel, command) -> int:  # Check if the command is a valid shell command
+        command = str(command)
+        return True
 
 # Provision SSH-based Honeypot
